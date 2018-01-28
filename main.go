3c22b35e98bd82d2552fba0bb40e70c740b2490c
Oliver Palmer <oliverpalmer@opalmer.com>
2018-01-28 15:09:02 -0500

    add -no-cache flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -8,7 +9,18 @@ import (
 	"github.com/opalmer/check-go-version/api"
 )
 
+var (
+	nocache = flag.Bool(
+		"no-cache", false,
+		"Disables caching of responses from Google object store.")
+)
+
 func main() {
+	flag.Parse()
+	if *nocache {
+		api.BucketCache = false
+	}
+
 	running, err := api.GetRunningVersion()
 	if err != nil {
 		log.Fatal(err)
